19e4dd33a37af772affca2c96ac47450db97a13f
lsepu <55538804+lsepu@users.noreply.github.com>
2022-06-17 00:37:09 -0500

    ADD: User route created

--- src/main/java/com/santiagoposadag/cs50/receiverpublisher/routers/CommandRouter.java
+++ src/main/java/com/santiagoposadag/cs50/receiverpublisher/routers/CommandRouter.java
@@ -2,7 +2,9 @@ package com.santiagoposadag.cs50.receiverpublisher.routers;
 
 
 import com.santiagoposadag.cs50.receiverpublisher.dto.CryptoCurrencyDto;
+import com.santiagoposadag.cs50.receiverpublisher.dto.UserDto;
 import com.santiagoposadag.cs50.receiverpublisher.usecases.PostMessageToRabbitUseCase;
+import com.santiagoposadag.cs50.receiverpublisher.usecases.PostUserToRabbitUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
@@ -33,6 +35,20 @@ public class CommandRouter {
         return route(POST("/SendAction")
                 .and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(CryptoCurrencyDto.class).flatMap(executor));
+
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> postUserActionRoute(PostUserToRabbitUseCase postUserToRabbitUseCase){
+        Function<UserDto, Mono<ServerResponse>> executor =
+                userDto -> postUserToRabbitUseCase.apply(userDto)
+                        .flatMap(result -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(result));
+
+        return route(POST("/SendUser")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(UserDto.class).flatMap(executor));
 
     }
 }
