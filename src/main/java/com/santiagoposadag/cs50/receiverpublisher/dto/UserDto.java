ef31ada1b2aa3f60fa8122134c500ba150f6df96
lsepu <55538804+lsepu@users.noreply.github.com>
2022-06-17 00:33:46 -0500

    ADD: DTO Created

---
+++ src/main/java/com/santiagoposadag/cs50/receiverpublisher/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.santiagoposadag.cs50.receiverpublisher.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto implements Serializable {
+    private String name;
+    private String routingKey;
+}
